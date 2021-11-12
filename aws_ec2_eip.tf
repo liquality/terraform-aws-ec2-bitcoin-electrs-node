e4e7889fdfbbcf7e24630d793f59f07004511225
|M| <manicprone@gmail.com>
2021-11-12 13:37:44 -0700

    Remove EIP

--- aws_ec2_eip.tf
+++ aws_ec2_eip.tf
@@ -1,14 +1,14 @@
 # ------------------------------------------------------------------------------
 # Elastic IP (for EC2)
 # ------------------------------------------------------------------------------
-resource "aws_eip" "lb" {
-  instance = aws_instance.main.id
-  vpc      = true
-
-  tags = merge(
-    local.tags,
-    {
-      Name = local.node_alias
-    }
-  )
-}
+# resource "aws_eip" "lb" {
+#   instance = aws_instance.main.id
+#   vpc      = true
+#
+#   tags = merge(
+#     local.tags,
+#     {
+#       Name = local.node_alias
+#     }
+#   )
+# }
