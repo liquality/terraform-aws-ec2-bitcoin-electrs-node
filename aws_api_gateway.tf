3b8f5e152a6c8d2853dd55fb16065273101e1ca6
|M| <manicprone@gmail.com>
2021-11-30 19:32:46 -0700

    Deploy api gateway

--- aws_api_gateway.tf
+++ aws_api_gateway.tf
@@ -1,52 +1,52 @@
-# # ------------------------------------------------------------------------------
-# # The API Gateway instance
-# # ------------------------------------------------------------------------------
-# resource "aws_api_gateway_rest_api" "main" {
-#   name        = local.node_alias
-#   description = "RPC Access for Bitcoin and Electrs"
-# }
-#
-# # ------------------------------------------------------------------------------
-# # The API Gateway deployment settings
-# # ------------------------------------------------------------------------------
-# resource "aws_api_gateway_deployment" "main" {
-#   rest_api_id = aws_api_gateway_rest_api.main.id
-#   stage_name  = var.env_alias
-#
-#   depends_on = [aws_api_gateway_integration.electrs_rpc]
-# }
-#
-# # ------------------------------------------------------------------------------
-# # Electrs RPC - API Gateway proxy (method + resource + integration)
-# # ------------------------------------------------------------------------------
-# resource "aws_api_gateway_resource" "electrs_rpc" {
-#   rest_api_id = aws_api_gateway_rest_api.main.id
-#   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-#   path_part   = "{proxy+}"
-# }
-#
-# resource "aws_api_gateway_method" "electrs_rpc" {
-#   rest_api_id   = aws_api_gateway_rest_api.main.id
-#   resource_id   = aws_api_gateway_resource.electrs_rpc.id
-#   http_method   = "ANY"
-#   authorization = "NONE"
-#
-#   request_parameters = {
-#     "method.request.path.proxy" = true
-#   }
-# }
-#
-# resource "aws_api_gateway_integration" "electrs_rpc" {
-#   rest_api_id = aws_api_gateway_rest_api.main.id
-#   resource_id = aws_api_gateway_resource.electrs_rpc.id
-#   http_method = aws_api_gateway_method.electrs_rpc.http_method
-#
-#   # Proxy all uri paths
-#   type                    = "HTTP_PROXY"
-#   integration_http_method = "ANY"
-#   uri                     = "http://${aws_eip.lb.public_ip}:3000/{proxy}"
-#
-#   request_parameters = {
-#     "integration.request.path.proxy" = "method.request.path.proxy"
-#   }
-# }
+# ------------------------------------------------------------------------------
+# The API Gateway instance
+# ------------------------------------------------------------------------------
+resource "aws_api_gateway_rest_api" "main" {
+  name        = local.node_alias
+  description = "RPC Access for Bitcoin and Electrs"
+}
+
+# ------------------------------------------------------------------------------
+# The API Gateway deployment settings
+# ------------------------------------------------------------------------------
+resource "aws_api_gateway_deployment" "main" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  stage_name  = var.env_alias
+
+  depends_on = [aws_api_gateway_integration.electrs_rpc]
+}
+
+# ------------------------------------------------------------------------------
+# Electrs RPC - API Gateway proxy (method + resource + integration)
+# ------------------------------------------------------------------------------
+resource "aws_api_gateway_resource" "electrs_rpc" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  parent_id   = aws_api_gateway_rest_api.main.root_resource_id
+  path_part   = "{proxy+}"
+}
+
+resource "aws_api_gateway_method" "electrs_rpc" {
+  rest_api_id   = aws_api_gateway_rest_api.main.id
+  resource_id   = aws_api_gateway_resource.electrs_rpc.id
+  http_method   = "ANY"
+  authorization = "NONE"
+
+  request_parameters = {
+    "method.request.path.proxy" = true
+  }
+}
+
+resource "aws_api_gateway_integration" "electrs_rpc" {
+  rest_api_id = aws_api_gateway_rest_api.main.id
+  resource_id = aws_api_gateway_resource.electrs_rpc.id
+  http_method = aws_api_gateway_method.electrs_rpc.http_method
+
+  # Proxy all uri paths
+  type                    = "HTTP_PROXY"
+  integration_http_method = "ANY"
+  uri                     = "http://${aws_eip.lb.public_ip}:3000/{proxy}"
+
+  request_parameters = {
+    "integration.request.path.proxy" = "method.request.path.proxy"
+  }
+}
