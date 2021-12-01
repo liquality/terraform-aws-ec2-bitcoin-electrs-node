78ab5f53c8e7d071128bb81c24448e0600128165
|M| <manicprone@gmail.com>
2021-11-30 18:55:31 -0700

    Add proxy for uri paths

--- aws_api_gateway.tf
+++ aws_api_gateway.tf
@@ -22,7 +22,7 @@ resource "aws_api_gateway_deployment" "main" {
 resource "aws_api_gateway_resource" "electrs_rpc" {
   rest_api_id = aws_api_gateway_rest_api.main.id
   parent_id   = aws_api_gateway_rest_api.main.root_resource_id
-  path_part   = "electrs"
+  path_part   = "electrs/{proxy+}"
 }
 
 resource "aws_api_gateway_method" "electrs_rpc" {
