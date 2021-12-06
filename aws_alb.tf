a165011b18b34727c87c2f250977b148ace86440
Devaraj Soundararajan <devarajsuk@gmail.com>
2021-12-06 11:08:37 +0530

    fix(alb): added depends_on for alb with the target group

--- aws_alb.tf
+++ aws_alb.tf
@@ -22,7 +22,7 @@ resource "aws_lb_listener" "main" {
   load_balancer_arn = aws_alb.main.arn
   port              = 80
   protocol          = "HTTP"
-
+  depends_on        = [aws_lb_target_group.main]
   default_action {
     type             = "forward"
     target_group_arn = aws_lb_target_group.main.arn
